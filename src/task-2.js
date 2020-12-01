5cdad3f6ed3ca9c33e3117917643864be7763838
Yuliia Semenets <yuliiasemenets123@gmail.com>
2020-12-01 19:19:19 +0200

    promisification

---
+++ src/task-2.js
@@ -0,0 +1,24 @@
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (users, userName) => {
+
+    return new Promise(resolve => {
+        for (const user of users) {
+            if (user.name === userName) {
+                user.active = !user.active;
+            }
+            resolve(users);
+        }
+    })
+};
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
